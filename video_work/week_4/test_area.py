41ecaf244bceb5d604169ec5823e0156c06cdab1
JRoble <89475216+Joeroble@users.noreply.github.com>
2022-09-14 13:45:33 -0500

    Triangle program end of video version

--- video_work/week_4/test_area.py
+++ video_work/week_4/test_area.py
@@ -11,6 +11,20 @@ class TestShapeAreas(TestCase):
     def test_triangle_area_floating_point(self):
         self.assertAlmostEqual(17.79875, area.triangle_area(7.25, 4.91))
 
+    def test_negative_base_height_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            area.triangle_area(-3, 0)
+        
+        with self.assertRaises(ValueError):
+            area.triangle_area(0, -3)
+        
+        with self.assertRaises(ValueError):
+            area.triangle_area(-3, -3)
+
+    def test_base_and_or_height_equal_zero(self):
+        self.assertEqual(0, area.triangle_area(0, 3))
+        self.assertEqual(0, area.triangle_area(3, 0))
+        self.assertEqual(0, area.triangle_area(0, 0))
     
 if __name__ == '__main__':
     unittest.main()
